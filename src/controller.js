cfe8053cbd86cf8868948767e4f47985c0e74a67
Pcoralie <Pcoralie@users.noreply.github.com>
2019-03-19 14:53:45 +0100

    feat(controller) : improved function

--- src/controller.js
+++ src/controller.js
@@ -1,82 +1,98 @@
 'use strict';
+const imdb = require('../src/imdb');
+
+const actor = 'nm0000243';
+//DENZEL_IMDB_ID
+
+require('../src/model');
 
 
 var mongoose = require('mongoose'),
-  Task = mongoose.model('Tasks');
+ Movie = mongoose.model('Movie');
 
-exports.list_all_tasks = function(req, res) {
-  Task.find({}, function(err, task) {
-    if (err)
+
+
+ //GET movies/populate
+exports.populate_the_db = async function(req, res){
+  const movies = await imdb(actor);
+  console.log(movies);
+
+    //Movie.save(movies);
+    Movie.insertMany(movies, function(err){
+  //  Movie.save(movies, function(err){
+      if(err)
+        res.send(err);
+      res.json(movies);
+    });
+  //src/imdb.js
+};
+
+//movies .get
+exports.must_watch_movies =  function(req, res){
+  //const awesome = movies.filter(movie => movie.metascore >= 70);
+  Movie.find({}, function(err){
+  if(err)
       res.send(err);
-    res.json(task);
+   res.json(movies);
   });
 };
 
 
 
-
-exports.create_a_task = function(req, res) {
-  var new_task = new Task(req.body);
-  new_task.save(function(err, task) {
+exports.list_all_movies = function(req, res) {
+  Movie.find({}, function(err, movie) {
     if (err)
       res.send(err);
-    res.json(task);
-  });
-};
-
-'use strict';
-
-
-var mongoose = require('mongoose'),
-  Task = mongoose.model('Tasks');
-
-exports.list_all_tasks = function(req, res) {
-  Task.find({}, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
+    res.json(movie);
   });
 };
 
 
 
-
-exports.create_a_task = function(req, res) {
-  var new_task = new Task(req.body);
-  new_task.save(function(err, task) {
+// movies  .post
+exports.create_a_movie = function(req, res) {
+  var new_movie = new Movie(req.body);
+  new_movie.save(function(err, movie) {
     if (err)
       res.send(err);
-    res.json(task);
+    res.json(movie);
   });
 };
 
 
-exports.read_a_task = function(req, res) {
-  Task.findById(req.params.taskId, function(err, task) {
+// movies/:id .get
+exports.read_a_movie = function(req, res) {
+  Movie.findById(req.params.id, function(err, movie) {
     if (err)
       res.send(err);
-    res.json(task);
+    res.json(movie);
   });
 };
 
-
-exports.update_a_task = function(req, res) {
-  Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
+// movies/:id .put
+exports.update_a_movie = function(req, res) {
+  Movie.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, function(err, movie) {
     if (err)
       res.send(err);
-    res.json(task);
+    res.json(movie);
   });
 };
 
-
-exports.delete_a_task = function(req, res) {
-
-
-  Task.remove({
-    _id: req.params.taskId
-  }, function(err, task) {
+// movies/ : id   .delete
+exports.delete_a_movie = function(req, res) {
+  Movie.remove({
+    _id: req.params.id
+  }, function(err, movie) {
     if (err)
       res.send(err);
-    res.json({ message: 'Task successfully deleted' });
+    res.json({ message: 'Movie successfully deleted' });
   });
+};
+
+//POST /movies/:id
+//save a watched date and a review
+//date - the watched date
+//review - the personal review
+exports.review_a_movie = function(req, res){
+  //Movie.
 };
